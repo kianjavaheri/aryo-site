ec87c844ee7896081bdc5ebc469117a467369d9c
Kian Javaheri <kianjavaheri911@gmail.com>
2022-12-19 17:17:46 -0800

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Activities from "./pages/Activities";
+import Hobbies from "./pages/Hobbies";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/hobbies" element={<Hobbies />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
     </div>
   );
 }
