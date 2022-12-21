ab299fed2cf2b957957c29473887820527dcdbe1
Kian Javaheri <kianjavaheri911@gmail.com>
2022-12-21 03:45:11 -0800

    12/21/22 - 3:45 AM

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,29 +19,24 @@ function Navbar() {
         <ul>
           <li><Link to="/" className="link">Home</Link></li>
           <li><Link to="/programs" className="link">University Programs</Link></li>
-          {/* <li><Link to="/activities" className="link">Activities</Link></li> */}
 
-          <li>
-            <a className="link" onClick={() => setOpen(!open)}>Activities</a>
-            {open && <DropdownMenu/>}
+          <li id="activities-dropdown">
+              <button className="link">Activities</button>
+              <div className="dropdown-content">
+                <div className="dropdown-item"><Link className="dd-link" to="/clubs">Clubs & Competitions</Link></div>
+                <div className="dropdown-item"><Link className="dd-link"to="/work">Internships & Work Experience</Link></div>
+                <div className="dropdown-item"><Link className="dd-link"to="/projects">Passion Project</Link></div>
+                <div className="dropdown-item"><Link className="dd-link"to="/volunteering">Volunteering & Leadership</Link></div>
+              </div>
           </li>
+
           <li><Link to="/hobbies" className="link">Hobbies</Link></li>
           <li><Link to="/resume" className="link">Resume</Link></li>
           <div id="sep"></div>
           <li><a href="https://www.instagram.com/aryo_garakani/" target="_blank" rel="noopener noreferrer" className="link"><FontAwesomeIcon icon={faInstagram} /></a></li>
           <li><a href="https://www.linkedin.com/in/aryo-garakani-a60511233/" target="_blank" rel="noopener noreferrer" className="link"><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
         </ul>
-    </div>
-  )
-}
 
-function DropdownMenu() {
-  return (
-    <div className="dropdown">
-      <li><Link to="/clubs" className="link">Clubs & Competitions</Link></li>
-      <li><Link to="/work" className="link">Internships & Work Experience</Link></li>
-      <li><Link to="/projects" className="link">Passion Project</Link></li>
-      <li><Link to="/volunteering" className="link">Volunteering & Leadership</Link></li>
     </div>
   )
 }
